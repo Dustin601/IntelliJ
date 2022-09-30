08325a1c837275cee83427ab5c507267ec7f7c5c
Dustin601 <dustin.nussi@gmail.com>
2022-09-30 09:31:34 +0200

    Osterformel not finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        int J = 2022;
+        int N = J - 1900;
+        int A = N % 19;
+        int B = ((7 * A + 1) / 19);
+        int M = (11 * A + 4 - B) % 29;
+        int Q = (N / 4);
+        int W = (N + Q + 31 - M) % 7;
+        int P = 25 - M - W;
+
+        if (P>0){
+            System.out.println("Osternsonntag ist am " + P + " April");
+        }
+
+
     }
 }
